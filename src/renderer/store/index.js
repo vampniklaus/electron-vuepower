befdbe50fc22d2f93ad10bb411511be4300bd972
vampniklaus <vampniklaus@aliyun.com>
2018-09-10 18:00:11 +0800

    111

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,11 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import modules from './modules'
+import user from './module/user'
+import app from './module/app'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules,
+  state: {
+    //
+  },
+  mutations: {
+    //
+  },
+  actions: {
+    //
+  },
+  modules: {
+    user,
+    app
+  },
   strict: process.env.NODE_ENV !== 'production'
 })
