6e17809e0cf18cc348d0298d041e16301d9b92da
vampniklaus <vampniklaus@aliyun.com>
2018-09-10 14:38:59 +0800

    添加

--- src/main/index.js
+++ src/main/index.js
@@ -47,8 +47,6 @@ app.on('activate', () => {
 })
 
 /**
- * Auto Updater  
- * 
  * Uncomment the following code below and install `electron-updater` to
  * support auto updating. Code Signing with a valid certificate is required.
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
